bb7d27b1b6e6d59c39fc519fb44bb71033daff6e
Hlumisa Mazomba <hmazomba@live.co.za>
2020-09-14 19:47:18 +0200

    Fixes
    Fixed a few typos and syntax errors. Moved the media and users folders. Admin can now add songs onto the website.

---
+++ api/music/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ListSongsView, PostSongsView
+from django.views.decorators.csrf  import csrf_exempt
+
+urlpatterns = [
+    path('songs/', ListSongsView.as_view(), name="songs-all"),
+    path('songs1/', csrf_exempt(PostSongsView.as_view()), name="songs-all")
+    ]
--- music/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Songs
-
-class SongSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Songs
-        fields = ("id", "title", "artist", "song", "uploader", "album")
